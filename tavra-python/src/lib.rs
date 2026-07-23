4cc94881881c2325728e9ed046caddc9a5cd39e5
Eira <eira@nornity.com>
2026-07-23 00:36:41 +0000

    feat: wire up Python module

--- tavra-python/src/lib.rs
+++ tavra-python/src/lib.rs
@@ -3,7 +3,32 @@
 
 use pyo3::prelude::*;
 
+mod binary;
+mod convert;
+mod envelope;
+mod schema;
+mod text;
+mod value;
+
 #[pymodule]
-fn tavra(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn tavra(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(text::parse, m)?)?;
+    m.add_function(wrap_pyfunction!(text::format, m)?)?;
+
+    m.add_function(wrap_pyfunction!(binary::encode, m)?)?;
+    m.add_function(wrap_pyfunction!(binary::decode, m)?)?;
+    m.add_function(wrap_pyfunction!(binary::hash, m)?)?;
+
+    m.add_function(wrap_pyfunction!(envelope::seal, m)?)?;
+    m.add_function(wrap_pyfunction!(envelope::open, m)?)?;
+    m.add_function(wrap_pyfunction!(envelope::generate_key, m)?)?;
+    m.add_function(wrap_pyfunction!(envelope::generate_signing_key, m)?)?;
+
+    m.add_function(wrap_pyfunction!(schema::validate, m)?)?;
+
+    m.add_function(wrap_pyfunction!(convert::from_json, m)?)?;
+    m.add_function(wrap_pyfunction!(convert::from_toml, m)?)?;
+    m.add_function(wrap_pyfunction!(convert::from_yaml, m)?)?;
+
     Ok(())
 }
